8bfab4ff867d4544724f8e43003dfcd9d93f56b3
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2023-04-17 08:32:29 +0100

    Added JS functionality
    Added JS functionality, counts and displays the score. Buttons are now working.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // javascript
+
+let homeScore = document.getElementById("home")
+let guestScore = document.getElementById("guest")
+let homeCount = 0
+let guestCount = 0
+
+function h1() {
+    homeCount += 1
+    homeScore.textContent = homeCount
+}
+function h2() {
+    homeCount += 2
+    homeScore.textContent = homeCount
+}
+function h3() {
+    homeCount += 3
+    homeScore.textContent = homeCount
+}
+
+function g1() {
+    guestCount += 1
+    guestScore.textContent = guestCount
+}
+function g2() {
+    guestCount += 2
+    guestScore.textContent = guestCount
+}
+function g3() {
+    guestCount += 3
+    guestScore.textContent = guestCount
+}
+
+
+if (guestScore < homeScore) {
+    document.getElementById("guest").classList.add(red);
+}
