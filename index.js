eecb89b91375451372a6c6d31415abc554e4b139
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2023-04-18 09:46:32 +0100

    Added colour to score cards
    When the score is more, the digits are green and when the score is lower, the digits are red.

--- index.js
+++ index.js
@@ -1,48 +1,128 @@
-let homeScore = document.getElementById("home")
-let guestScore = document.getElementById("guest")
-let homeCount = 0
-let guestCount = 0
+let homeScore = document.getElementById("home");
+let guestScore = document.getElementById("guest");
+let homeCount = 0;
+let guestCount = 0;
 
 function h1() {
-    homeCount += 1
-    homeScore.textContent = homeCount
-    if (homeCount > guestCount) {
-        document.getElementById("guest").classList.remove(score);
-        document.getElementById("guest").classList.add(red);
-
-    }
+  homeCount += 1;
+  homeScore.textContent = homeCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
+  // both green
 }
 function h2() {
-    homeCount += 2
-    homeScore.textContent = homeCount
+  homeCount += 2;
+  homeScore.textContent = homeCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
 }
 function h3() {
-    homeCount += 3
-    homeScore.textContent = homeCount
+  homeCount += 3;
+  homeScore.textContent = homeCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
 }
 
 function g1() {
-    guestCount += 1
-    guestScore.textContent = guestCount
+  guestCount += 1;
+  guestScore.textContent = guestCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
 }
 function g2() {
-    guestCount += 2
-    guestScore.textContent = guestCount
+  guestCount += 2;
+  guestScore.textContent = guestCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
 }
 function g3() {
-    guestCount += 3
-    guestScore.textContent = guestCount
+  guestCount += 3;
+  guestScore.textContent = guestCount;
+  let updatedHome = homeCount;
+  let updatedGuest = guestCount;
+  if (updatedHome > guestCount) {
+    //guest is red
+    document.getElementById("guest").className = "red";
+    document.getElementById("home").className = "green";
+  } else if (updatedHome < guestCount) {
+    //home is red
+    document.getElementById("home").className = "red";
+    document.getElementById("guest").className = "green";
+  } else {
+    document.getElementById("home").className = "green";
+    document.getElementById("guest").className = "green";
+  }
 }
-
 
 if (guestScore < homeScore) {
-    document.getElementById("guest").classList.add(red);
+  document.getElementById("guest").classList.add(red);
 }
 
-function reset () {
-    homeCount = 0
-    guestCount = 0
-    homeScore.textContent = 0
-    guestScore.textContent = 0
-  
+function reset() {
+  homeCount = 0;
+  guestCount = 0;
+  homeScore.textContent = 0;
+  guestScore.textContent = 0;
+  document.getElementById("home").className = "green";
+  document.getElementById("guest").className = "green";
 }
